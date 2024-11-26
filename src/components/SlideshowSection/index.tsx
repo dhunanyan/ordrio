e206bff01d86b519e12be7ccb8ef63426538d7e5
dhunanyan <davit.hunanyan@me.com>
2024-11-26 19:12:07 +0100

    fix: resolve slideshow issue

--- src/components/SlideshowSection/index.tsx
+++ src/components/SlideshowSection/index.tsx
@@ -10,21 +10,26 @@ export type SlideshowSectionPropsType = {
 };
 
 export const SlideshowSection = ({ imageURLs }: SlideshowSectionPropsType) => (
-  <ul className="slideshow-section">
-    {(
-      Array(10) // This is to duplicate the pictures to ensure that on big screens we also show something
-        .fill(imageURLs)
-        .reduce((prev, next) => prev.concat(next)) as string[]
-    ).map((imageURL, i) => (
-      <motion.li
-        key={i}
-        className="slideshow-section__item"
-        initial={{ opacity: 0, x: -120, y: 10 }}
-        whileInView={{ opacity: 1, x: 0, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.1 + i * 0.05 }}
-      >
-        <img src={imageURL} alt={`Image ${i}`} />
-      </motion.li>
+  <motion.div
+    className="slideshow-section"
+    initial={{ opacity: 0, x: -120, y: 10 }}
+    whileInView={{ opacity: 1, x: 0, y: 0 }}
+    transition={{ duration: 0.5, delay: 0.1 }}
+  >
+    {[
+      ...Array(3), // This one is for infinite loop effect
+    ].map((_, i) => (
+      <ul key={i} className="slideshow-section__list">
+        {(
+          Array(10) // This one is for bigger screens
+            .fill(imageURLs)
+            .reduce((prev, next) => prev.concat(next)) as string[]
+        ).map((imageURL, j) => (
+          <li key={j} className="slideshow-section__item">
+            <img src={imageURL} alt={`Image ${i}`} />
+          </li>
+        ))}
+      </ul>
     ))}
-  </ul>
+  </motion.div>
 );
